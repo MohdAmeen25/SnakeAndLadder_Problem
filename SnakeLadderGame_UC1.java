2e2a5a508656b8ccfa76f09b4904b523007bceed
MohdAmeen25 <ameen25mohd@gmail.com>
2023-01-06 00:25:18 +0530

    UC1 Solved

---
+++ SnakeLadderGame_UC1.java
@@ -0,0 +1,9 @@
+public class SnakeLadderGame_UC1 {
+    public static void main(String[] args) {
+
+        final int Starting_position = 0;
+        System.out.println("Starting position is :" + Starting_position);
+
+
+    }
+}
