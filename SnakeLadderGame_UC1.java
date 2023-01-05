1252c8d6f90f7229da0656c366ca46e75295e5ff
Mohd Ameen <116932302+MohdAmeen25@users.noreply.github.com>
2023-01-06 00:53:58 +0530

    Delete SnakeLadderGame_UC1.java

--- SnakeLadderGame_UC1.java
+++
@@ -1,9 +0,0 @@
-public class SnakeLadderGame_UC1 {
-    public static void main(String[] args) {
-
-        final int Starting_position = 0;
-        System.out.println("Starting position is :" + Starting_position);
-
-
-    }
-}
