a5d523dd97c86a6d4934fd60846fc3767af74635
MohdAmeen25 <ameen25mohd@gmail.com>
2023-01-06 00:29:44 +0530

    UC2 Solved

---
+++ SnakeLadderGame_UC2.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class SnakeLadderGame_UC2 {
+    public static void main(String[] args) {
+        int Starting_position = 0;
+        System.out.println("Starting position is : " + Starting_position);
+
+        Random random = new Random();
+        int	Rolling_Die = random.nextInt(6)+1;
+        System.out.println(Rolling_Die);
+    }
+}
